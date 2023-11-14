16433b82b3ee0a0a5b8063b9203266a65b789d7c
codermahbub20 <mkrefat5@gmail.com>
2023-11-15 00:26:16 +0600

    Footer section are created

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -14,7 +14,7 @@ const Rooms = () => {
     },[])
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-7">
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-7">
             {
                 rooms?.map(room => <Card key={room._id} room={room}></Card>)
             }
