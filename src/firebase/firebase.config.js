6df7f578ff4f78ef2dea0de74b350321f8bc12f6
codermahbub20 <mkrefat5@gmail.com>
2023-11-14 21:35:00 +0600

    Project setup complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvHai4Y7KhJcje6Nnmz7J80Pp29gnPwUM",
+  authDomain: "stay-vista-9d5b3.firebaseapp.com",
+  projectId: "stay-vista-9d5b3",
+  storageBucket: "stay-vista-9d5b3.appspot.com",
+  messagingSenderId: "371935319125",
+  appId: "1:371935319125:web:c16e1abc16cfd842e870dc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
